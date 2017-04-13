f913f68e5233bdaf210064fe5b9572cbc9382141
Maksymilian <darkey999@yahoo.com>
2017-04-13 14:36:10 +0200

    Initial commit

---
+++ app/src/main/java/com/example/gosu/admobtest/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.gosu.admobtest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
