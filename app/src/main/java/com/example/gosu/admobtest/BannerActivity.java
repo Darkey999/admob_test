7cbc4bea88582353471839ff56ec2a4d85e86990
Maksymilian <darkey999@yahoo.com>
2017-04-13 15:02:18 +0200

    Add request for ad

--- app/src/main/java/com/example/gosu/admobtest/BannerActivity.java
+++ app/src/main/java/com/example/gosu/admobtest/BannerActivity.java
@@ -3,11 +3,22 @@ package com.example.gosu.admobtest;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
 public class BannerActivity extends AppCompatActivity {
+    private AdView adView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_banner);
+
+        adView = (AdView) findViewById(R.id.adView);
+
+        // Create request for ad
+        AdRequest adRequest = new AdRequest.Builder().build();
+        adView.loadAd(adRequest);
+
     }
 }
